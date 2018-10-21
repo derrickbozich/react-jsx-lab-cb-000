998b3594989a6741e134b24cbce7f2f4a5d79069
derrick b <dbozich1@gmail.com>
2018-10-21 04:13:06 +0000

    automatically backed up by learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -1,9 +1,9 @@
-
-
-class FillerText extends Component {
+export class FillerText extends Component {
   render(){
     return (
       <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
     )
   }
 }
+
+export default reveal
